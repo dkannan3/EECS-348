74a30821d33ce799dba2891af9cfaa0cac469b6e
dkannan3 <dkannan0703@gmail.com>
2023-10-15 22:10:15 -0500

    Lab 5

---
+++ lab5/fb.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void find_combinations(int score, int current_score, char* path, int current_play) {
+    int plays[] = {8, 7, 6, 3, 2};  // Corresponding point values for TD+2pt, TD+FG, Touchdown, 3pt FG, and Safety
+    char play_names[][20] = {"TD + 2pt", "TD + FG", "TD", "3pt + FG", "Safety"};
+    
+    if (score == current_score) {
+        printf("%s\n",path);
+        return;
+    }
+
+    for (int i = current_play; i < 5; i++) {
+        if (current_score + plays[i] <= score) {
+            char new_path[100];
+            if (current_score > 0) {
+                snprintf(new_path, sizeof(new_path), "%s, %s", path, play_names[i]);
+            } else {
+                snprintf(new_path, sizeof(new_path), "%s", play_names[i]);
+            }
+            find_combinations(score, current_score + plays[i], new_path, i);
+        }
+    }
+}
+
+int main() {
+    int score = 25;
+    printf("Enter the NFL score: %d\n", score);
+
+    printf("Possible combinations of scoring plays:\n");
+    char path[100] = "";
+    find_combinations(score, 0, path, 0);
+
+    return 0;
+}
---
+++ lab5/sales.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_MONTHS 12
+
+// Define a structure to hold sales values and months
+struct SalesData {
+    char month[10];
+    double sales;
+};
+
+void readMonthlySales(double sales[NUM_MONTHS], const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening the file");
+        exit(1);
+    }
+
+    for (int i = 0; i < NUM_MONTHS; i++) {
+        if (fscanf(file, "%lf", &sales[i]) != 1) {
+            fprintf(stderr, "Error reading sales data from the file");
+            exit(1);
+        }
+    }
+
+    fclose(file);
+}
+
+void calculateSalesSummary(double sales[NUM_MONTHS], double *min, double *max, double *avg) {
+    *min = sales[0];
+    *max = sales[0];
+    *avg = sales[0];
+
+    for (int i = 1; i < NUM_MONTHS; i++) {
+        if (sales[i] < *min) {
+            *min = sales[i];
+        }
+        if (sales[i] > *max) {
+            *max = sales[i];
+        }
+        *avg += sales[i];
+    }
+
+    *avg /= NUM_MONTHS;
+}
+
+void generateSalesReport(double sales[NUM_MONTHS]) {
+    printf("Monthly sales report for 2022:\n");
+    printf("Month\tSales\n");
+    const char *monthNames[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+    for (int i = 0; i < NUM_MONTHS; i++) {
+        printf("%-12s\t$%.2lf\n", monthNames[i], sales[i]);
+    }
+}
+
+void generateMovingAverageReport(double sales[NUM_MONTHS]) {
+    printf("\nSix-Month Moving Average Report:\n");
+    printf("Month Range Average\n");
+    const char *monthNames[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+    for (int i = 0; i <= NUM_MONTHS - 6; i++) {
+        double sum = 0;
+        for (int j = i; j < i + 6; j++) {
+            sum += sales[j];
+        }
+        printf("%s - %s   \t$%.2lf\n", monthNames[i], monthNames[i + 5], sum / 6);
+    }
+}
+
+// Comparison function for qsort
+int compareSales(const void *a, const void *b) {
+    return ((struct SalesData *)b)->sales - ((struct SalesData *)a)->sales;
+}
+
+void generateHighestToLowestReport(double sales[NUM_MONTHS]){
+    char *months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+
+    // Create an array of SalesData structures to associate each sales value with its corresponding month
+    struct SalesData salesData[NUM_MONTHS];
+    for (int i = 0; i < NUM_MONTHS; i++) {
+        strcpy(salesData[i].month, months[i]);
+        salesData[i].sales = sales[i];
+    }
+    qsort(salesData, NUM_MONTHS, sizeof(struct SalesData), compareSales);
+
+    // Print the sorted data with the corresponding months
+    printf("\nSales Report (Highest to Lowest):\n");
+    printf("Month      \tSales\n");
+    for (int i = 0; i < NUM_MONTHS; i++) {
+        printf("%s      \t$%.2f\n", salesData[i].month, salesData[i].sales);
+    }
+}
+
+int main() {
+    double sales[NUM_MONTHS];
+    readMonthlySales(sales, "sales.txt");
+
+    double min, max, avg;
+    calculateSalesSummary(sales, &min, &max, &avg);
+
+    generateSalesReport(sales);
+
+    printf("\nSales summary:\n");
+    printf("Minimum sales: $%.2lf\n", min);
+    printf("Maximum sales: $%.2lf\n", max);
+    printf("Average sales: $%.2lf\n", avg);
+
+    generateMovingAverageReport(sales);
+    generateHighestToLowestReport(sales);
+    return 0;
+}
